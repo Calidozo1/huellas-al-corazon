7b8d42bad643fa9be2b591645f8c9b9eae5a14f0
Calidozo1 <acardozo164@gmail.com>
2026-04-26 22:03:15 -0400

    arreglo rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ajuste automático del base path para GitHub Pages
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  // Usamos una ruta base relativa para que funcione automáticamente en cualquier subdirectorio de GitHub Pages
+  base: './',
   build: {
     outDir: 'docs' // Construimos en la carpeta docs para servir directamente desde la rama principal
   }
