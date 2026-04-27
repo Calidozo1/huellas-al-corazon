d379b6a2307239d9c6e90303a58a7ccf712057f1
Calidozo1 <acardozo164@gmail.com>
2026-04-26 21:44:48 -0400

    github pages sync

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ajuste automático del base path para GitHub Pages
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
 })
